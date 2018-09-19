471800371bcd4a1c23754d6ff9ac3d13b41cfd63
Ben Del Vacchio <delvach@gmail.com>
2018-09-19 16:44:20 -0600

    Pushing API key for heroku configuration without delving into config options.

---
+++ src/config/prod.js
@@ -0,0 +1,5 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB3PQWAPBSs6LXbN7sroY48quEFXsqUds0',
+  authDomain: 'user-visualizer.firebaseapp.com',
+  databaseURL: 'https://user-visualizer.firebaseio.com'
+};
